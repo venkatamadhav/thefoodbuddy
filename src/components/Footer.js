2732d7c78f8c30f870de7696249ef95d76c07605
venkatamadhav <venkat.madhav.98@gmail.com>
2023-06-14 20:45:54 +0530

    Added header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        TheFoodBuddy &#xA9; 2023
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="heading">
+        <h1>TheFoodBuddy</h1>
+      </div>
+      <div className="navbar-links">
+        <ul>
+          <li>Home</li>
+          <li>Categories</li>
+          <li>Random</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
