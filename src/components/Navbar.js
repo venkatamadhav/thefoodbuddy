00195a13a0ae330cd373e0b39cafa9367f18d130
venkatamadhav <venkat.madhav.98@gmail.com>
2023-06-18 00:27:57 +0530

    Fixed Word wrap and added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from "../Logo.png"
 
 const Navbar = () => {
   return (
     <div className="bg-black text-white flex justify-between items-center w-full h-14 sticky top-0">
-      <div className="ml-4 text-3xl">
-        <h1>The Food <span className='text-orange-300'>Buddy</span></h1>
-      </div>
+      <Link to="/">
+        <div className="ml-4">
+          {/* <h1>The Food <span className='text-orange-300'>Buddy</span></h1> */}
+          <img src={Logo} className="h-12" alt='logo'/>
+        </div>
+      </Link>
       <div className="navbar-links">
         <ul className='flex'>
           <Link to="/"><li className='mr-6 text-xl bg-[#5c5c5c] p-2.5 rounded transition-all ease duration-300 hover:text-amber-400'>Home</li></Link>
